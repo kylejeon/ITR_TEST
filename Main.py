bd567de1a9f42e14eb36a7c3a53d4ad75e042778
kylejeon <kylejeon83@gmail.com>
2022-12-01 09:53:54 +0900

    .

--- Main.py
+++ Main.py
@@ -164,11 +164,8 @@ def full_test():
                     failed_test_list.remove(test)
                     break
                 except:
-                    # 새로고침
                     ITR_Admin_Common.driver.refresh()
-
                     print("An exception occurred.")
-                    ITR_Admin_Common.driver.refresh()
                     pass
         finally:
             print("Run Time:", round((int(time.time() - run_time)/60),2),"min\n")
